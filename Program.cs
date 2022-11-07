d0d67d88ac1e1757e9b2a97c0dc39f7f3035a6f2
EruslanovAndrei <eruslanov84as@gmail.com>
2022-11-07 15:36:55 +0300

    add program Bubble Sort

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Данны (BubbleSort) метод может сортировать массив от меньшего к большему и 
+// наоборот,для изменения результата
+// необходимо просто поменять знак (<) на (>) внутри метода BubbleSort!
+
+Console.Clear();
+int size = 25;
+int[] array = new int[size];
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write("]");
+    }
+}
+
+void BubbleSort(int[] arr)
+{
+    var n = array.Length;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+
+        }
+    }
+}
+
+FillArray(array);
+PrintArray(array);
+BubbleSort(array);
+Console.WriteLine();
+PrintArray(array);
